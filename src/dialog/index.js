c0fec00c4c1a8d835ddd02e6fa5dab26557f2565
Fab <fabou291@gmail.com>
2022-12-19 08:20:56 +0100

    🔨 Add html and js relative to dialog dialog component

--- src/dialog/index.js
+++ src/dialog/index.js
@@ -1,7 +1,37 @@
-const button = document.querySelector("button");
+const button = {
+  showModal: document.querySelector("#showModal"),
+  close: document.querySelector("#close"),
+  cancel: document.querySelector("#cancel"),
+};
 const dialog = document.querySelector("dialog");
 const content = document.querySelector("dialog>div");
+const body = document.querySelector("body");
 
-button.addEventListener("click", () => dialog.showModal(), false);
-dialog.addEventListener("click", () => dialog.close(), false);
+function isDialogSupported() {
+  return typeof HTMLDialogElement === "function";
+}
+
+function show() {
+  dialog.showModal();
+  body.classList.add("scroll-muted");
+}
+
+function close() {
+  dialog.close();
+  const dialogOpened = document.querySelectorAll("dialog[open]");
+  if (!dialogOpened.length) body.classList.remove("scroll-muted");
+}
+
+button.showModal.addEventListener("click", show, false);
+button.close.addEventListener("click", close, false);
+button.cancel.addEventListener(
+  "click",
+  () => {
+    dialog.returnValue = null;
+    close();
+  },
+  false
+);
+dialog.addEventListener("click", close, false);
+dialog.addEventListener("cancel", () => (dialog.returnValue = null), false);
 content.addEventListener("click", (e) => e.stopPropagation(), false);
---
+++ src/dialog/readme.md
@@ -0,0 +1,24 @@
+# Dialog
+
+<table>
+  <tr>
+    <th>Question</th>
+    <th>Support</th>
+  </tr>
+  <tr>
+    <td>transition possible ?</td>
+    <td>?</td>
+  </tr>
+  <tr>
+    <td>Depassement vertical possible ?</td>
+    <td>?</td>
+  </tr>
+  <tr>
+    <td>Depassement horizontal possible ?</td>
+    <td>?</td>
+  </tr>
+  <tr>
+    <td>form method dialog</td>
+    <td>?</td>
+  </tr>
+</table>
